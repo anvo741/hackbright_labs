db13aa01b8812d74a84e2c3e63f53fbd36aca162
vagrant <vagrant@vagrant.vm>
2019-10-22 02:58:40 +0000

    Refactor produce summary

---
+++ melon-delivery-report/produce_summary.py
@@ -0,0 +1,18 @@
+def melon_delivery_report(day, order_file):
+    the_file = open(order_file)
+    print("Day {}".format(day))    
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+            
+        print("Delivered {} {}s for total of ${}".format(
+                count, melon, amount))
+    the_file.close()
+
+melon_delivery_report(1, "um-deliveries-20140519.txt")
+melon_delivery_report(2, "um-deliveries-20140520.txt")
+melon_delivery_report(3, "um-deliveries-20140521.txt")
